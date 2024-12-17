1d989e051699a2e5cd1a18feb44659ff7b4745b4
anEnemyStand <clintonc1997@gmail.com>
2024-12-16 21:44:06 -0500

    added more exercises

--- sql-bolt-exercises.sql
+++ sql-bolt-exercises.sql
@@ -283,89 +283,50 @@ GROUP BY Director;
  
 	--  SQL Lesson 13: Inserting rows  ------------------------------
     
--- 1.
+-- 1. Add the studio's new production, Toy Story 4 to the list of movies (you can use any director) 
+INSERT INTO Movies
+(Title,Director)
+VALUES ("Toy Story 4", "John Lasseter");
 
 
--- 2.
-
-
--- 3.
-
-
--- 4.
- 
- 
-    
-	--  SQL Lesson 6: Multi-table queries with JOINs  ------------------------------
-    
--- 1.
-
-
--- 2.
-
-
--- 3.
-
-
--- 4.
- 
- 
-	--  SQL Lesson 6: Multi-table queries with JOINs  ------------------------------
-    
--- 1.
-
-
--- 2.
-
-
--- 3.
-
-
--- 4.
- 
- 
-  
-	--  SQL Lesson 6: Multi-table queries with JOINs  ------------------------------
-    
--- 1.
-
-
--- 2.
-
-
--- 3.
-
-
--- 4.
- 
- 
- 
-	--  SQL Lesson 6: Multi-table queries with JOINs  ------------------------------
-    
--- 1.
-
-
--- 2.
-
-
--- 3.
-
-
--- 4.
- 
+/* 2. Toy Story 4 has been released to critical acclaim! It had a rating of 8.7, and made 
+	340 million domestically and 270 million internationally. Add the record to the BoxOffice table.  */
+INSERT INTO Boxoffice
+(Movie_id, Rating, Domestic_sales, International_sales)
+VALUES (15, 8.7, 340000000, 270000000);
  
     
-	--  SQL Lesson 6: Multi-table queries with JOINs  ------------------------------
+	--  SQL Lesson 14: Updating rows  ------------------------------
     
--- 1.
+-- 1. The director for A Bug's Life is incorrect, it was actually directed by John Lasseter 
+UPDATE Movies
+SET Director = "John Lasseter"
+WHERE Title = "A Bug's Life";
 
 
--- 2.
+-- 2. The year that Toy Story 2 was released is incorrect, it was actually released in 1999 
+Update Movies
+SET Year = 1999
+WHERE Title = 'Toy Story 2';
 
 
--- 3.
+/* 3. Both the title and director for Toy Story 8 is incorrect! The title should be 
+	"Toy Story 3" and it was directed by Lee Unkrich */
+UPDATE Movies
+SET Title = "Toy Story 3",
+    Director = "Lee Unkrich"
+WHERE Title = "Toy Story 8";
 
-
--- 4.
  
- 
+	--  SQL Lesson 15: Deleting rows  ------------------------------
+    
+-- 1. This database is getting too big, lets remove all movies that were released before 2005. 
+DELETE FROM Movies
+WHERE Year < 2005;
+
+
+-- 2. Andrew Stanton has also left the studio, so please remove all movies directed by him. 
+DELETE FROM Movies
+WHERE Director = "Andrew Stanton";
+
+
